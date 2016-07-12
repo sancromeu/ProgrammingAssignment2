53921918c11ee0387a31cf1171e96954f7d42814
me <sancromeu@users.noreply.github.com>
2016-07-12 02:34:09 -0400

    Assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of utility functions to calculate and cache a matrix inverse.
+## Caching is achieved by using generic "objects" with accessors that 
+## implemented to work under R's lexical scoping
 
-## Write a short comment describing this function
+## Sample execution:
+#  $> source("cachematrix.R")
+#  $> m <- matrix(c(2, 4, 3, 1, 5, 7, 2,7,4), nrow = 3, ncol = 3)
+#  $> cm <- makeCacheMatrix(m)
+#  $> cm$inverse$get()    <<<< returns NULL
+#  $> cacheSolve(cm)      <<<< first invocation, message indicates caching operation (miss)
+#  $> cm$inverse$get()    <<<< shows inverse
+#  $> cacheSolve(cm)      <<<< subsequent invocation, no caching message (hit)
+#  $> m2 <- matrix(c(1, 2, 3, 1, 5, 7, 2,7,4), nrow = 3, ncol = 3)
+#  $> cm$matrix$set(m2)   <<<< resets cache internally
+#  $> cm$inverse$get()    <<<< returns NULL
+#  $> cacheSolve(cm)      <<<< first invocation, message indicates caching operation (miss)
+#  $> cm$inverse$get()    <<<< shows inverse
+#  $> cacheSolve(cm)      <<<< subsequent invocation, no caching message (hit)
 
+#
+# Creates an object that holds a matrix and its inverse 
+# Both the matrix and the inverse are objects of type dc
+#
+# To retrieve matrix do: mCM$matrix$get()
+# To retrieve metrix inverse do: mCM$inverse$get()
+#
+# Note that inverse will be null until you invoke cacheSolve(mCM)
+# Note that resetting the matrix will also reset the inverse
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- dc(NULL)
+    m <- dcfun(x, function() { inv$set(NULL) } )
+    list(matrix = m, inverse = inv)
 }
 
 
-## Write a short comment describing this function
+#
+# Returns the inverse or the matrix held by the provided makeCacheMatrix
+# Matrix inverse is calculated and cached for future invocations
+#
+cacheSolve <- function(x) {
+    slv <- x$inverse$get()
+    if (is.null(slv)) {
+        print("Calculating and caching inverse")
+        slv <- solve(x$matrix$get())
+        x$inverse$set(slv)
+    }
+    slv
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+#
+# The most basic data container with accessor methods
+#
+dc <- function(x = numeric()) {
+    set <- function(y) x <<- y
+    get <- function() x
+    list(set = set, get = get)
+}
+
+
+#
+# A data container that executes function provided on set invocation
+#
+dcfun <- function(x = numeric(), onset = NULL, ...) {
+    set <- function(y) {
+        x <<- y
+        if (!is.null(onset)) {
+            onset(...)
+        }
+    }
+    get <- function() x
+    list(set = set, get = get)
 }
